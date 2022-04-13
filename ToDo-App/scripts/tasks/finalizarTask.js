7b4a5abcd09a35c2cb7728f6923a25b3505113fc
Matheus_Almeida <matheus.almeida.sobrinho@hotmail.com>
2022-04-13 17:30:34 -0300

    funcao finalizar tela tarefa

---
+++ ToDo-App/scripts/tasks/finalizarTask.js
@@ -0,0 +1,5 @@
+let finalizarPaginaTasks = document.getElementById('closeApp')
+
+finalizarPaginaTasks.addEventListener('click', function () {
+  location.href = 'index.html'
+})
--- ToDo-App/scripts/tasks/tasks.js
+++ ToDo-App/scripts/tasks/tasks.js
@@ -1,46 +1,41 @@
-let tokenJwt = sessionStorage.getItem("jwt");
+let tokenJwt = sessionStorage.getItem('jwt')
 
 const taskObjeto = {
-  description: "teste",
+  description: 'teste',
   completed: false
 }
 
-let TaskJson = JSON.stringify(taskObjeto);
+let TaskJson = JSON.stringify(taskObjeto)
 
-
-let btAddTask = document.getElementById("btAddTask");
+let btAddTask = document.getElementById('btAddTask')
 
 //Executa ao clicar no botão de Acessar
 
-
 function addTask(taskJson) {
-
   //Executar o acesso a API com o login
-  let urlEndPointAddTask = "https://ctd-todo-api.herokuapp.com/v1/tasks";
+  let urlEndPointAddTask = 'https://ctd-todo-api.herokuapp.com/v1/tasks'
   let configDaRequisicao = {
-      method: 'POST',
-      headers: {
-          'content-type': 'application/json',
-          'authorization': tokenJwt
-      },
-      body: taskJson
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      authorization: tokenJwt
+    },
+    body: taskJson
   }
 
   fetch(urlEndPointAddTask, configDaRequisicao)
-  .then(
-      resultado => {
-          if (resultado.status == 201) {
-          return resultado.json();
+    .then(resultado => {
+      if (resultado.status == 201) {
+        return resultado.json()
       }
-      throw resultado;
-  }).then(
-      resultado => {
-          console.log(resultado);
-  }).catch(
-      erro => {
-      console.log(erro);
-  });
-
+      throw resultado
+    })
+    .then(resultado => {
+      console.log(resultado)
+    })
+    .catch(erro => {
+      console.log(erro)
+    })
 }
 
-btAddTask.addEventListener('click', addTask());
+btAddTask.addEventListener('click', addTask())
